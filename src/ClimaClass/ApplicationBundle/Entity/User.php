692a14370d76c62e26c6f36072851cdbb6365983
Licence <Licence@134.157.46.187>
2015-06-18 11:28:22 +0200

    Debut mon compte !

--- src/ClimaClass/ApplicationBundle/Entity/User.php
+++ src/ClimaClass/ApplicationBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace ClimaClass\ApplicationBundle\Entity;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * User
@@ -106,6 +107,46 @@ class User extends BaseUser
      * @ORM\OneToMany(targetEntity="Report", mappedBy="user")
      **/
     private $reports;
+    
+    private $file;
+    
+    public function getFile() {
+
+        return $this->file;
+    }
+
+    public function setFile(UploadedFile $file = null) {
+
+        $this->file = $file;
+    }
+
+    public function upload() {
+        // Si jamais il n'y a pas de fichier (champ facultatif), on ne fait rien
+        if (null === $this->file) {
+            return;
+        }
+
+        // On récupère le nom original du fichier de l'internaute
+        $name = $this->file->getClientOriginalName();
+
+
+        // On déplace le fichier envoyé dans le répertoire de notre choix
+        $this->file->move($this->getUploadRootDir(), $name);
+
+        // On sauvegarde le nom de fichier dans notre attribut $url
+        $this->picture = $name;
+
+    }
+    
+     public function getUploadDir() {
+        // On retourne le chemin relatif vers l'image pour un navigateur (relatif au répertoire /web donc)
+        return 'bundles/climatclassapplication/images/users';
+    }
+
+    protected function getUploadRootDir() {
+        // On retourne le chemin relatif vers l'image pour notre code PHP
+        return __DIR__ . '/../../../../web/' . $this->getUploadDir();
+    }
     
     public function __construct() {
         parent::__construct();
---
+++ src/ClimaClass/ApplicationBundle/Form/AccountType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ClimaClass\ApplicationBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class AccountType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        // add your custom field
+        $builder
+                ->add('establishment')
+                ->add('class')
+                ->add('lastname')
+                ->add('firstname')
+                ->add('description')
+                ->add('file','file',array('required'=>false))
+                ->add('main_language', 'entity', array('property' => 'language', "class" => 'ClimaClass\ApplicationBundle\Entity\Language'))
+                ->add('languages', 'entity', array('property' => 'language', "class" => 'ClimaClass\ApplicationBundle\Entity\Language', 'multiple' => true))
+        ;
+    }
+
+    public function getParent() {
+        return 'fos_user_profile';
+    }
+
+    public function getName() {
+        return 'climaclass_user_profile';
+    }
+
+}
