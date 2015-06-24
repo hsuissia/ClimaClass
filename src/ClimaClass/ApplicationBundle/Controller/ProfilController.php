c35fce28771bad109cdf6bc9e8d84cc0e4ade45a
Licence <Licence@134.157.46.187>
2015-06-24 17:33:11 +0200

    Start month

--- src/ClimaClass/ApplicationBundle/Controller/ProfilController.php
+++ src/ClimaClass/ApplicationBundle/Controller/ProfilController.php
@@ -20,11 +20,15 @@ class ProfilController extends Controller {
         $tabrainlevel = array();
         $tmpraintab = array();
         $tabwind = array();
+        
         $class = $this->getDoctrine()->getRepository("ClimaClassApplicationBundle:User")->find($id);
         $reports = $this->getDoctrine()->getRepository("ClimaClassApplicationBundle:Report")->findByUser($class, array('postDate' => 'desc'));
+        
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate($reports, $request->query->getInt('page', 1), 3);
+        
         $reportsInYear = $this->getDoctrine()->getRepository("ClimaClassApplicationBundle:Report")->findReportForCharts($class);
+        
         $next_month = date("Y-m-d", strtotime(date('Y-m-d') . "- 1years + 1month "));
         $datetime_year_before = new \DateTime($next_month);
         $datetime_today = new \DateTime(date('Y-m-d'));
@@ -32,7 +36,8 @@ class ProfilController extends Controller {
             $tmpraintab[$datetime_year_before->format('F')] = null;
             $datetime_year_before->modify("+ 1months");
         }
-        $start_month = $datetime_today->format('F');
+        $start_month = $datetime_year_before->format('Y-m');
+        echo $start_month;
         foreach ($reportsInYear as $report) {
             foreach ($report->getMeasures() as $measures) {
                 if ($measures->getTemperature() != "") {
